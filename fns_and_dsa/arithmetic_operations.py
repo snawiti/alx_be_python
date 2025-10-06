8fce0d8af346c7886bdd3cd6d53486d9645b40f4
snawiti <40319524+snawiti@users.noreply.github.com>
2025-10-06 13:30:51 +0200

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 * num2
     elif operation == "divide":
         if num2 == 0:
-            return "Division by zero is not allowed"
+            return "Division by zero error"
         return num1 / num2
     else:
         return "Invalid operation"
