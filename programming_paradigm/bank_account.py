053d2e96052388ea8eea145a60e6aa8420f6a13a
snawiti <snawiti@yahoo.com>
2025-10-09 16:40:46 +0200

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -25,4 +25,4 @@ class BankAccount:
 
     def display_balance(self):
         """Display the current account balance."""
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current Balance: ${self.__account_balance:.2f}")
